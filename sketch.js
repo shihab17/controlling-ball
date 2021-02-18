71280ff940663805af69dd894c04bd63ad33e127
shihab17 <56069831+shihab17@users.noreply.github.com>
2021-02-19 00:56:15 +0600

    change

--- sketch.js
+++ sketch.js
@@ -1,19 +1,22 @@
 let bird;
 let obs = [];
 function setup() {
-    createCanvas(windowWidth, windowHeight-100);
+    createCanvas(windowWidth, windowHeight - 200);
     let r = random(255);
     let g = random(255);
     let b = random(255);
-    background(r,g,b)
+    background(r, g, b)
     bird = new Bird();
     obs.push(new Obstacle());
     // Align text center both horizontally and vertically.
-  textAlign(CENTER, CENTER);
-  textSize(50);
+    textAlign(CENTER, CENTER);
+    textSize(50);
+    button = createButton('reload');
+ button.position(100, 20);
+ button.mousePressed(reload);
 }
 function draw() {
-    background(255)
+    background(65,105,225)
     bird.update();
     for (let i = 0; i < obs.length; i++) {
         if (obs[i].collides(bird)) {
@@ -25,12 +28,15 @@ function draw() {
     }
     if (frameCount % 70 == 0) {
         console.log(frameCount)
-        document.getElementById("point").innerText =frameCount;
+        document.getElementById("point").innerText = frameCount;
         text(frameCount);
-            obs.push(new Obstacle());
+        obs.push(new Obstacle());
     }
     bird.show();
 }
 function mousePressed() {
     bird.up();
+}
+function reload(){
+    location.reload();
 }
